15900d70ad91658a2bd3e6a38f24840d3b5f101b
ARTISTCI-BEAST <artisticbeast1@gmail.com>
2023-08-25 19:35:44 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
-# Install puppet-lint package
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  name     => 'puppet-lint'
-  provider => 'gem',
+# installs puppet-lint package
+
+package {'flask':
+  ensure   => '2.1.0',
+  name     => 'flask',
+  provider => 'pip3'
 }
