31070c216915a736ff81e43cec80ecff32a453bc
ARTISTCI-BEAST <artisticbeast1@gmail.com>
2023-09-10 12:09:24 -0700

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -11,7 +11,7 @@ file_line { 'Redirection':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => '        rewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;'
+  line   => '        rewrite ^/redirect_me https://www.github.com/ARTISTIC-BEAST permanent;'
 }
 service { 'nginx':
   ensure    => running,
