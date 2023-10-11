cae2d975af1725ec7b9aa1c714bc73453f4e3d02
ARTISTIC-BEAST <artisticbeast1@gmail.com>
2023-10-10 19:05:44 -0700

    2-recurse.py

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" 0. How many subs? """
+
+from requests import get
+
+headers = {'User-Agent': 'My User Agent'}
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    """Number of subs function"""
+
+    url = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(
+        subreddit, after)
+    response = get(url, headers=headers, allow_redirects=False)
+    if response.status_code is not 200:
+        return None
+    r = response.json()
+    data = r.get('data')
+    children = data['children']
+    for i in range(len(children)):
+        hot_list.append(children[i].get('data')['title'])
+    after = data['after']
+    if after is not None:
+        recurse(subreddit, hot_list, after)
+    return hot_list
